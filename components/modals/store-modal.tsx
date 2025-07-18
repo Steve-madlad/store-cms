95ab59daaeb75bf88e17b2a6c8c16e27a3146248
Estifanos Wendimu <teffsauce2@gmail.com>
2025-07-19 02:55:59 +0300

    added dependancies & components, modified store modal component, ts config, & gobal styles, integrated prisma

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -2,9 +2,37 @@
 
 import { useModalStore } from "@/hooks/useModalStore";
 import Modal from "../ui/custom/modal";
+import * as zod from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
 
 export default function StoreModal() {
   const storeModal = useModalStore();
+
+  const formSchema = zod.object({
+    name: zod.string().min(2, "Store name is required"),
+  });
+
+  const form = useForm<zod.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const onSubmit = async (values: zod.infer<typeof formSchema>) => {
+    alert(JSON.stringify(values, null, 2));
+  };
 
   return (
     <Modal
@@ -14,6 +42,32 @@ export default function StoreModal() {
       onClose={storeModal.onClose}
     >
       Create Store Form
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="mt-4 space-y-3">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="E-Commerce" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <div className="flex justify-end gap-2">
+            <Button type="button" variant={"outline"}>
+              Cancel
+            </Button>
+            <Button className="cursor" type="submit">
+              Submit
+            </Button>
+          </div>
+        </form>
+      </Form>
     </Modal>
   );
 }
